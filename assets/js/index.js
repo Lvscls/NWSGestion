f024f7cb71a44e35549a2906d7594f41df73e25b
Lilian Vasconcelos <lilian.vasconcelos@outlook.fr>
2022-11-12 10:42:30 +0100

    [FIX] header CORS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,95 @@
 
-$("#add_material").submit(function(event){
-    alert("Data inserted Successfully")
-})
 
-$("#update_material").submit(function(event){
-    event.preventDefault()
+$("#add_material").submit(function (event) {
+  alert("Data inserted Successfully");
+});
 
-    var unindexed_array = $(this).serializeArray()
-    var data = {}
+$("#update_material").submit(function (event) {
+  event.preventDefault();
 
-    $.map(unindexed_array, function(n,i){
-        data[n['name']] = n['value']
-    })
-    console.log(data)
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
 
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+  console.log(data);
+
+  var request = {
+    url: `http://localhost:3000/api/materials/${data.id}`,
+    header: 'Access-Control-Allow-Origin',
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (response) {
+    alert("Data modified Successfully");
+  });
+});
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
     var request = {
-        "url": `http://localhost:3000/api/materials/${data.id}`,
-        "method": "PUT",
-        "data": data
+      url: `http://localhost:3000/api/materials/${id}`,
+      header: 'Access-Control-Allow-Origin',
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this ?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data deleted Successfully");
+        location.reload();
+      });
     }
-
-    $.ajax(request).done(function(response){
-        alert("Data modified Successfully")
-    })
-})
-
-if(window.location.pathname == '/'){
-    $ondelete = $('.table tbody td a.delete')
-    $ondelete.click(function(){
-        var id = $(this).attr("data-id")
-        var request = {
-            "url": `http://localhost:3000/api/materials/${id}`,
-            "method": "DELETE",
-        }
-
-        if(confirm("Do you really want to delete this ?")){
-            $.ajax(request).done(function(response){
-                alert("Data deleted Successfully")
-                location.reload()
-            })
-        }
-    })
+  });
 }
 
-if(window.location.pathname == '/loans'){
-    $ondelete = $('.table tbody td a.delete')
-    $ondelete.click(function(){
-        var id = $(this).attr("data-id")
-        var request = {
-            "url": `http://localhost:3000/api/loans/${id}`,
-            "method": "DELETE",
-        }
+if (window.location.pathname == "/loans") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+    var request = {
+      url: `http://localhost:3000/api/loans/${id}`,
+      header: 'Access-Control-Allow-Origin',
+      method: "DELETE",
+    };
 
-        if(confirm("Do you really want to delete this ?")){
-            $.ajax(request).done(function(response){
-                alert("Data deleted Successfully")
-                location.reload()
-            })
-        }
-    })
+    if (confirm("Do you really want to delete this ?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data deleted Successfully");
+        location.reload();
+      });
+    }
+  });
+
+  $onsend = $(".table tbody td a.mail");
+  $onsend.click(function () {
+    var id = $(this).attr("data-id");
+    var request = {
+      url: `http://localhost:5000/api/loans?${id}`,
+      header: 'Access-Control-Allow-Origin',
+      method: "GET",
+    };
+
+    {
+      $.ajax(request).done(function (response) {
+        const requiredPath = path.join(__dirname, "../../views/sendMail.ejs");
+
+        const data = ejs.renderFile(requiredPath, {
+          loan: response,
+        });
+        var mainOptions = {
+          from: '"NormandieWebSchool" normandiewebschool@gmail.com',
+          to: response.email,
+          subject: "Rappel",
+          html: data,
+        };
+        alert("Mail send Successfully");
+        transporter.sendMail(mainOptions);
+        location.reload();
+      });
+    }
+  });
 }
