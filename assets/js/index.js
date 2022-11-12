5ac8410b6ab2e818bccda3af55a01b25490df234
Lilian Vasconcelos <lilian.vasconcelos@outlook.fr>
2022-11-12 17:28:10 +0100

    [FIX] fetch cors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,3 @@
-
-
 $("#add_material").submit(function (event) {
   alert("Data inserted Successfully");
 });
@@ -9,47 +7,52 @@ $("#update_material").submit(function (event) {
 
   var unindexed_array = $(this).serializeArray();
   var data = {};
-
   $.map(unindexed_array, function (n, i) {
     data[n["name"]] = n["value"];
   });
-  console.log(data);
 
+  console.log(data);
   var request = {
     url: `http://localhost:3000/api/materials/${data.id}`,
     method: "PUT",
     data: data,
   };
-
+  console.log(request);
   $.ajax(request).done(function (response) {
     alert("Data modified Successfully");
   });
+  // const reference = document.querySelector('input[name="reference"]');
+  // const description = document.querySelector('input[name="description"]');
+  // console.log(reference.value);
+  // console.log(description.value);
+
+  // fetch(`http://localhost:3000/api/materials/${data.id}`, {
+  //   method: "PUT",
+  //   headers: {
+  //     ContentType: 'application/json',
+  //     Accept: 'application/json',// Indicates the content
+  //   },
+  //   body: JSON.stringify({
+  //     '_id': data.id,
+  //     'reference': reference,
+  //     'description': description,
+  //     'statut': false
+  //   })
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
 });
 
 if (window.location.pathname == "/") {
   $ondelete = $(".table tbody td a.delete");
   $ondelete.click(function () {
     var id = $(this).attr("data-id");
-    var request = {
-      url: `http://localhost:3000/api/materials/${id}`,
-      contentType: "application/json",
-      headers: {
-        "accept": "application/json",
-        "Access-Control-Allow-Origin":"https://lilian.iamroot.fr"
-    } ,
-      crossDomain: true,
-      dataType: "jsonp",
-      xhrFields: { withCredentials: true },
+    fetch(`http://localhost:3000/api/materials/${id}`, {
       method: "DELETE",
-      jsonp: "callback"
-    };
-
-    if (confirm("Do you really want to delete this ?")) {
-      $.ajax(request).done(function (response) {
-        alert("Data deleted Successfully");
-        location.reload();
-      });
-    }
+      headers: {
+        "Access-Control-Allow-Origin": "https://lilian.iamroot.fr",
+      },
+    }).then(alert("Data deleted Successfully"), location.reload());
   });
 }
 
@@ -57,44 +60,39 @@ if (window.location.pathname == "/loans") {
   $ondelete = $(".table tbody td a.delete");
   $ondelete.click(function () {
     var id = $(this).attr("data-id");
-    var request = {
-      url: `http://localhost:3000/api/loans/${id}`,
+    fetch(`http://localhost:3000/api/loans/${id}`, {
       method: "DELETE",
-    };
-
-    if (confirm("Do you really want to delete this ?")) {
-      $.ajax(request).done(function (response) {
-        alert("Data deleted Successfully");
-        location.reload();
-      });
-    }
+      headers: {
+        "Access-Control-Allow-Origin": "https://lilian.iamroot.fr",
+      },
+    }).then((res) => alert("Data deleted Successfully"), location.reload());
   });
 
-  $onsend = $(".table tbody td a.mail");
-  $onsend.click(function () {
-    var id = $(this).attr("data-id");
-    var request = {
-      url: `http://localhost:5000/api/loans?${id}`,
-      method: "GET",
-    };
+  // $onsend = $(".table tbody td a.mail");
+  // $onsend.click(function () {
+  //   var id = $(this).attr("data-id");
+  //   var request = {
+  //     url: `http://localhost:5000/api/loans?${id}`,
+  //     method: "GET",
+  //   };
 
-    {
-      $.ajax(request).done(function (response) {
-        const requiredPath = path.join(__dirname, "../../views/sendMail.ejs");
+  //   {
+  //     $.ajax(request).done(function (response) {
+  //       const requiredPath = path.join(__dirname, "../../views/sendMail.ejs");
 
-        const data = ejs.renderFile(requiredPath, {
-          loan: response,
-        });
-        var mainOptions = {
-          from: '"NormandieWebSchool" normandiewebschool@gmail.com',
-          to: response.email,
-          subject: "Rappel",
-          html: data,
-        };
-        alert("Mail send Successfully");
-        transporter.sendMail(mainOptions);
-        location.reload();
-      });
-    }
-  });
+  //       const data = ejs.renderFile(requiredPath, {
+  //         loan: response,
+  //       });
+  //       var mainOptions = {
+  //         from: '"NormandieWebSchool" normandiewebschool@gmail.com',
+  //         to: response.email,
+  //         subject: "Rappel",
+  //         html: data,
+  //       };
+  //       alert("Mail send Successfully");
+  //       transporter.sendMail(mainOptions);
+  //       location.reload();
+  //     });
+  //   }
+  // });
 }
