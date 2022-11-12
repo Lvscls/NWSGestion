e6a22ae07137903db31e906a4be6e522a78c66b0
Lilian Vasconcelos <lilian.vasconcelos@outlook.fr>
2022-11-12 10:57:33 +0100

    [FIX] dataType json in CORS

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,6 +34,7 @@ if (window.location.pathname == "/") {
       url: `http://localhost:3000/api/materials/${id}`,
       contentType: "application/json",
       crossDomain: true,
+      dataType: "json",
       method: "DELETE",
     };
 
