a2c750e676409615952722d41477c64a0b29ba30
Lilian Vasconcelos <lilian.vasconcelos@outlook.fr>
2022-11-12 11:32:28 +0100

    [TEST] node cors option

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ connectDB();
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors({ origin: "https://lilian.iamroot.fr", credentials: true }));
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 //set view engine
 app.set("view engine", "ejs");
